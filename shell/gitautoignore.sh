2d5e35c748490ddf17a33306e9922ce3dcca2f8f
Wayne Doucette <doucette.wayne@gmail.com>
2011-09-07 19:34:14 -0700

    GitAutoIgnore updates .gitignore properly now.

--- shell/gitautoignore.sh
+++ shell/gitautoignore.sh
@@ -8,7 +8,9 @@
 #/											#
 #/											#
 #/	Usage:										#
-#/	./gitautoignore will append list to .gitignore 					#
+#/											#
+#/	Do: ./gitautoignore to append list to .gitignore in current directory		#
+#/											#
 #/											#
 #////////////////////////////////////////////////////////////////////////////////////////
 ######################################################################################
@@ -16,10 +18,19 @@
 
 
 # Exit on error.
-set -e
+#set -e
 
 # TODO SEARCHPATH should be relative ./
 SEARCHPATH=./ 
+COMMENT="GITAUTOIGNORE"
+MIME_TYPE="application\/x-exe"
+
+# Remove existing entires by filtering COMMENT and clobbering .gitignore
+if [ -s ".gitignore" ]
+then 
+cat .gitignore | grep -v $COMMENT > /tmp/.gitignore
+mv /tmp/.gitignore ./
+fi
 
 # Recures SEARCHPATH to produce tree of files to be tested.
 find $SEARCHPATH -name "*" -print  > /tmp/.gitautoignore
@@ -30,7 +41,8 @@ find $SEARCHPATH -name "*" -print  > /tmp/.gitautoignore
 # Test each file for matching MIME type and output .gitignore file in current working directory 
 
 # grep --exclude-from=.gitignore
-file --mime-type -f /tmp/.gitautoignore | grep -h application\/x-exe | cut -d : -f 1 | sed "s/^\.\/\(.*\)/\1 #AUTOIGNORE/" >> .gitignore
+# Test MIME type from tmp tree file, strip leading ./ on files in current dir. Append list with #comments.
+file --mime-type -f /tmp/.gitautoignore | grep -h $MIME_TYPE | cut -d : -f 1 | sed "s/^\.\/\(.*\)/\1\t#${COMMENT}/" >> .gitignore
 
 #FILES=`file --mime-type -f /tmp/.gitautoignore` 
 #FILES=echo $FILES | grep -h application\/x-exe 
